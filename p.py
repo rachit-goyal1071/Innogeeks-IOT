7ae72b2e9f33008a41ff03207a34cbb2d0837725
Rachit Goyal <rg410345@gmail.com>
2023-12-05 14:03:32 +0530

    changes

--- p.py
+++ p.py
@@ -1,2 +1,13 @@
-s = "KIET Delhi-NCR"
-print(s[0:len(s):2])
+def create_file(filename):
+    file = open(filename, 'w')
+    content = input("Enter the content:")
+    file.write(content)
+    file.close()
+    
+    f = open(filename, 'r')
+    print(f.read())
+    f.close()
+
+# Test the function
+create_file('new_file.txt')
+
